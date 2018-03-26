cf970e9fad0c56a8c98944bac0de1a43740e8945
Kaye Milton <kaye2milton@gmail.com>
2018-03-26 11:03:43 +0300

    Updated default period

--- src/contents/stock_status.php
+++ src/contents/stock_status.php
@@ -1295,7 +1295,7 @@ function stock_status(){
             $onerow = pg_fetch_array(pg_query($db, $cur));
 
             $org = 'akV6429SUqu'; $orgname = 'Uganda';
-            $per = '2018W11'; //$onerow['yr'] . 'W' . $onerow['weekno'];
+            $per = '2018W12'; //$onerow['yr'] . 'W' . $onerow['weekno'];
             $yr = 2018; //$onerow['yr'];
             $wk = 12; //$onerow['weekno'];
 
